f5d516d645c94a14a602dd66dfe940cb00991db7
tanonkim <robbins4bos@gmail.com>
2023-02-09 23:18:20 +0900

    Feature: footer

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 import Nav from './components/Nav/Nav';
 
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
+
       <Routes></Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
