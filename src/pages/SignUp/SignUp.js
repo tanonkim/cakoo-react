7eee409c35cbd270b3b9058a200cc557f42e5f8f
김기현 <tanonkim@gimgihyeon-ui-noteubug.local>
2023-02-11 21:57:26 +0900

    Feature: sign-up design

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -47,7 +47,7 @@ function SignUp() {
           </div>
           <div className="row">
             <label>휴대폰 번호</label>
-            <div>
+            <div className="phone">
               <input
                 type="text"
                 placeholder="010"
@@ -74,7 +74,7 @@ function SignUp() {
           </div>
           <div className="row">
             <label>생년월일</label>
-            <div>
+            <div className="bitrh">
               <input
                 type="text"
                 placeholder="년도) 2023"
@@ -83,6 +83,61 @@ function SignUp() {
               <input type="text" placeholder="월" className="birthMonth" />
               <input type="text" placeholder="일" className="birthDay" />
             </div>
+          </div>
+          <hr />
+          <div className="termBox">
+            <span>이용약관 동의</span>
+            <div className="termCheckBox">
+              <ul>
+                <li className="list">
+                  <label className="conditionTrue">
+                    <span>
+                      <input type="checkbox"></input>
+                      <span>모두 동의합니다.</span>
+                    </span>
+                  </label>
+                </li>
+                <li className="list">
+                  <label className="conditionTrue">
+                    <span>
+                      <input type="checkbox"></input>
+                      <span>이용약관 동의</span>
+                      <b>(필수)</b>
+                    </span>
+                  </label>
+                </li>
+                <li className="list">
+                  <label className="conditionTrue">
+                    <span>
+                      <input type="checkbox"></input>
+                      <span>개인정보 수집, 이용동의</span>
+                      <b>(필수)</b>
+                    </span>
+                  </label>
+                </li>
+                <li className="list">
+                  <label className="conditionTrue">
+                    <span>
+                      <input type="checkbox"></input>
+                      <span>이벤트, 혜택정보 수신 동의</span>
+                      <b>(선택)</b>
+                    </span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="document">
+            <a href="/" type="button">
+              이용약관 보기
+            </a>
+            <span> · </span>
+            <a href="/" type="button">
+              개인정보 처리방침 보기
+            </a>
+          </div>
+          <div className="signUpBtn">
+            <button type="button">회원가입</button>
           </div>
         </form>
       </fieldset>
