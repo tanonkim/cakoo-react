eb7fb184c393e1c48489cde70ec5c3c4d31af246
김기현 <tanonkim@gimgihyeon-ui-noteubug.local>
2023-02-12 21:08:57 +0900

    Feature: Add button change by regex

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignIn.scss';
 
 function SignIn() {
+  const emailRegex =
+    /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
   const [signInInfo, setSignInInfo] = useState({
     email: '',
     password: '',
@@ -12,6 +18,19 @@ function SignIn() {
     const { name, value } = e.target;
     setSignInInfo({ ...signInInfo, [name]: value });
     console.log(name, value);
+  };
+
+  const isValid = () => {
+    if (
+      emailRegex.test(signInInfo.email) &&
+      passwordRegex.test(signInInfo.password) &&
+      signInInfo.password.length >= 8
+    ) {
+      return true;
+    } else {
+      console.log('fail');
+      return false;
+    }
   };
 
   return (
@@ -33,13 +52,23 @@ function SignIn() {
       </div>
       <div className="button">
         <label>
-          <input type="checkbox" name="" value="" /> 아이디 저장
+          <input type="checkbox" /> 아이디 저장
         </label>
       </div>
-      <button className="loginButton">로그인</button>
+      <button
+        type="button"
+        className={isValid() ? 'loginButtonOn' : 'loginButton'}
+        disabled={!isValid()}
+      >
+        로그인
+      </button>
       <div className="etc">
-        <span>아이디 찾기</span>
-        <span>비밀번호 찾기</span>
+        <Link to="/">
+          <span>아이디 찾기</span>
+        </Link>
+        <Link to="/">
+          <span>비밀번호 찾기</span>
+        </Link>
         <Link to="/signup">
           <span>회원가입</span>
         </Link>
