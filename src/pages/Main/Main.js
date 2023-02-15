0a56c806e0b2f7fe88c6539e7c4f68275b646ee3
tanonkim <robbins4bos@gmail.com>
2023-02-16 00:08:19 +0900

    Fix: Change structure for sizing

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import API from '../../config';
 import './Main.scss';
 import Banner from './Banner/Banner';
-import FilterForm from './FilterForm/FilterForm';
 import Product from './Product/Product';
 
 // http://0.0.0.0:8000/products?sort=recent&limit=8&offset=10
@@ -24,10 +23,39 @@ function Main() {
     setOffset(prev => prev + 1);
   }
 
+  function handleCheck(event) {
+    console.log(event);
+  }
+
   return (
     <main>
       <Banner />
-      <FilterForm />
+      <div className="filterBar">
+        <form className="filterForm">
+          <label>
+            <input type="checkbox" name="mini" onChange={handleCheck} />
+            mini
+          </label>
+          <label>
+            <input type="checkbox" name="one" onChange={handleCheck} />
+            1호
+          </label>
+          <label>
+            <input type="checkbox" name="two" onChange={handleCheck} />
+            2호
+          </label>
+          <label>
+            <input type="checkbox" name="three" onChange={handleCheck} />
+            3호
+          </label>
+        </form>
+        <div>
+          <button type="button">가격 높은순</button>
+          <button type="button">가격 낮은순</button>
+          <button type="button">신상품</button>
+          <button type="button">오래된 순</button>
+        </div>
+      </div>
       <div className="productContainer">
         {productList.map(data => (
           <Product key={data.id} data={data} />
