a3da6027f87a8a4fb3632d2cbc1f7da09a0c7b57
tanonkim <robbins4bos@gmail.com>
2023-02-15 09:34:24 +0900

    Feature: Add Basic layout

--- src/pages/Main/Product/Product.js
+++ src/pages/Main/Product/Product.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import './Product.scss';
 
 function Product({ data }) {
   const {
@@ -18,6 +19,20 @@ function Product({ data }) {
         <img alt="상품 미리보기" className="thumbnail" src={thumbnail} />
       </Link>
       <span className="productInfo"> {description}</span>
+      <span className="productName"> {name}</span>
+      <span className="productPriceInfo">
+        <span> {discount_rate * 100}% </span>
+        <span> {price} </span>
+        <span> {price - discount_price} 원 </span>
+      </span>
+      <div className="productOtherInfo">
+        <span className="productSizes">
+          {sizes.map((size, index) => (
+            <span key={index}> {size} </span>
+          ))}
+        </span>
+        <span>무료배송</span>
+      </div>
     </article>
   );
 }
