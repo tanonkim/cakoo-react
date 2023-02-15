96d943f8f40fe46cd0b3ccf78e78a44487935627
tanonkim <robbins4bos@gmail.com>
2023-02-15 11:15:41 +0900

    Feature: Add layout design

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,15 +5,24 @@ import Banner from './Banner/Banner';
 import FilterForm from './FilterForm/FilterForm';
 import Product from './Product/Product';
 
+// http://0.0.0.0:8000/products?sort=recent&limit=8&offset=10
+// products: `${BASE_URL}/products`,
+
 function Main() {
   const [productList, setProductList] = useState([]);
+  const [offset, setOffset] = useState(0);
+
   const baseUri = API.products;
 
   useEffect(() => {
-    fetch(baseUri)
+    fetch(`${baseUri}?offset=0&limit=${(offset + 1) * 8}`)
       .then(res => res.json())
       .then(result => setProductList(result.lists));
   }, []);
+
+  // function leadMore() {
+  //   setOffset(prev => console.log(prev));
+  // }
 
   return (
     <main>
@@ -24,7 +33,11 @@ function Main() {
           <Product key={data.id} data={data} />
         ))}
       </div>
-      <button className="leadMore">더보기</button>
+      <div className="buttonBox">
+        <button className="leadMore" onClick="">
+          더보기
+        </button>
+      </div>
     </main>
   );
 }
