58b5bb39861104560b90a2cad6995058c7da0764
tanonkim <robbins4bos@gmail.com>
2023-02-17 17:20:57 +0900

    Feature: add data in detail page

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,25 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ProductDetail.scss';
+import API from '../../config';
+import { useParams } from 'react-router-dom';
 
 function ProductDetail() {
-  // const [product, setProduct] = useState({});
+  const [product, setProduct] = useState({});
+
+  const params = useParams();
+  const { id } = params; // params.id
+
+  useEffect(() => {
+    fetch(`${API.products}/${id}`)
+      .then(res => res.json())
+      .then(result => {
+        setProduct(result.message);
+        // console.log(result);
+      });
+  }, []);
+
+  const {
+    base_price,
+    description,
+    discount_rate,
+    information_images,
+    name,
+    product_images,
+    sizes,
+  } = product;
 
   return (
     <main className="productBox">
       <div className="topBox">
         <div className="img">
-          <div> `Home > 케이크 > name`</div>
+          <div> Home > 케이크 > {name}</div>
           <img
-            src="/images/strawberry_biscuit_cake.jpeg"
+            src={product_images[0]}
             alt="상품 이미지 크게 보기"
             className="mainImage"
           />
         </div>
         <div className="goCart">
-          <p className="description">testDescription</p>
+          <p className="description">{description}</p>
 
           <div className="productInfo">
-            <span className="discountRate">20%</span>
+            <span className="discountRate">{discount_rate * 100}%</span>
             <del className="netPrice">10000원</del>
             <span className="costPrice">8000원</span>
             <hr />
